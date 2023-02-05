950394ed922f77457b288c101345abe01738420f
Isac Lima <isacl1996@gmail.com>
2023-02-05 15:22:59 -0300

    Added a UI to the game

--- rps.js
+++ rps.js
@@ -1,43 +1,148 @@
-// All of the possible shapes.
-const SHAPES = ["rock", "paper", "scissors"];
+// Used to decide the winner.
+const SHAPE_ORDER = { "rock": 0, "paper": 1, "scissors": 2 };
 
-// Makes a random choice of index.
+// Used to choose a random shape.
+const SHAPES = Object.keys(SHAPE_ORDER);
+
+// The game's scoreboard.
+let SCOREBOARD = { "player": 0, "computer": 0 };
+
+// Choose a random shape.
 function getComputerChoice() {
-    return Math.floor(SHAPES.length * Math.random());
+    const index = Math.floor(SHAPES.length * Math.random());
+    return SHAPES[index];
 }
 
-// Takes the indices chosen by the player and the computer.
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        alert(`Draw! I chose ${SHAPES[computerSelection]} too!`)
-    }
-    // A bit of modular arithmetic.
-    else if ((playerSelection + 1) % 3 == computerSelection % 3) {
-        alert(`You lose! I chose ${SHAPES[computerSelection]}!`);
+// Determined who scored based on the player's and the computer's choices.
+function getScorer(playerChoice, computerChoice) {
+    const playerNumber = SHAPE_ORDER[playerChoice];
+    const computerNumber = SHAPE_ORDER[computerChoice];
+
+    if (playerNumber == computerNumber) {
+        return null;
+    } else if ((playerNumber + 1) % 3 == computerNumber % 3) {
+        return "computer";
     } else {
-        alert(`You win! I chose ${SHAPES[computerSelection]}!`);
+        return "player";
     }
 }
 
-function game() {
-    // Play 5 rounds of rock-paper-scissors.
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = -1;
-        // Keep asking for a choice of shape until the player quits on enters a valid shape.
-        while (playerSelection == -1) {
-            let playerShape = prompt("Which shape do you choose: rock, paper or scissors?");
-            if (playerShape == null) {
-                return;
-            }
-            playerSelection = SHAPES.indexOf(playerShape.toLocaleLowerCase());
-            if (playerSelection == -1) {
-                alert(`"${playerShape}" is not a valid shape!`);
-            }
-        }
-        // Get computer choice and play the round.
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
+// Insert the current scores into the scoreboard HTML.
+function updateScoreboard() {
+    const playerScore = document.querySelector("#player-score");
+    const computerScore = document.querySelector("#computer-score");
+    playerScore.innerHTML = SCOREBOARD["player"];
+    computerScore.innerHTML = SCOREBOARD["computer"];
+}
+
+// Update the prompt based on the scoreboard.
+function updatePrompt() {
+    const prompt = document.querySelector("#prompt");
+
+    // If the player scored 5 points, then they win.
+    if (SCOREBOARD["player"] == 5) {
+        prompt.innerHTML = `
+            You scored 5 points! You win!<br>
+            Click the <strong>restart</strong> button to play again
+        `;
     }
+    // Else, if the computer scored 5 points, then it wins.
+    else if (SCOREBOARD["computer"] == 5) {
+        prompt.innerHTML = `
+            I scored 5 points! I win!<br>
+            Click the <strong>restart</strong> button to play again
+        `;
+    }
+    // Otherwise, just ask the player for input.
+    else {
+        prompt.innerHTML = "Choose a shape:";
+    }
+}
+
+function displayResults(playerChoice, computerChoice, scorer) {
+
+    // Update the scorer message based on the round's results.
+    const scorerMessage = document.querySelector("#scorer-message");
+    switch (scorer) {
+        case null:
+            scorerMessage.innerHTML = "Draw!";
+            break;
+        case "player":
+            scorerMessage.innerHTML = "You score!";
+            break;
+        case "computer":
+            scorerMessage.innerHTML = "I score!";
+            break;
+    }
+
+    // Update the image and caption for the player's and the computer's choices.
+    const playerChoiceImage = document.querySelector("#player-choice img");
+    const playerChoiceCaption = document.querySelector("#player-choice .caption");
+    const computerChoiceImage = document.querySelector("#computer-choice img");
+    const computerChoiceCaption = document.querySelector("#computer-choice .caption");
+
+    playerChoiceImage.className = playerChoice;
+    playerChoiceCaption.innerHTML = `You chose ${playerChoice}`;
+    computerChoiceImage.className = computerChoice;
+    computerChoiceCaption.innerHTML = `I chose ${computerChoice}`;
+
+    // Unhide the section that shows the round's results.
+    const roundResults = document.querySelector("#round-results");
+    roundResults.classList.remove("is-hidden");
+}
+
+// Hide the section that shows the round's results.
+function hideResults() {
+    const roundResults = document.querySelector("#round-results");
+    roundResults.classList.add("is-hidden");
+}
+
+// Play a round of rock paper scissors given the player's and the computer's choice.
+function playRound(playerChoice, computerChoice) {
+
+    // Only do something if neither the player nor the computer have scored 5 points yet.
+    if (!Object.values(SCOREBOARD).includes(5)) {
+        // The determine who scored and increment their score if necessary.
+        const scorer = getScorer(playerChoice, computerChoice);
+        if (scorer != null) {
+            SCOREBOARD[scorer]++;
+        }
+        // Update the HTML based on the current scoredboard and the chosen shapes.
+        updateScoreboard();
+        updatePrompt()
+        displayResults(playerChoice, computerChoice, scorer);
+    }
+}
+
+// Reset the scoreboard and update the HTML elements.
+function restart() {
+    SCOREBOARD["player"] = 0;
+    SCOREBOARD["computer"] = 0;
+    updateScoreboard();
+    updatePrompt();
+    hideResults();
+}
+
+// Player a 5-round rock paper scissors game.
+function game() {
+
+    // Set up the scoreboard and the HTML elements.
+    restart();
+
+    // Buttons containing the shapes for the player to choose.
+    const shapeButtons = Array.from(document.querySelectorAll("#shape-selector button"));
+    // Button to restart the game.
+    const restartButton = document.querySelector("#restart-button");
+
+    // Each shape button will have an event listener.
+    // Whenever the player clicks one of them (thus, choosing a shape), a new round is played.
+    shapeButtons.forEach(
+        b => b.addEventListener(
+            "click", e => playRound(b.className, getComputerChoice())
+        )
+    );
+    // Event listener that allows the restart button to actually restart the game.
+    restartButton.addEventListener("click", restart);
 }
 
 game();
